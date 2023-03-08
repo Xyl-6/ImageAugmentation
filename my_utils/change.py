a5606eca7acaba9acb68b3cba12a71b571807035
Xyl-6 <1129563391@qq.com>
2023-03-08 19:55:31 +0800

    before_flip

--- my_utils/change.py
+++ my_utils/change.py
@@ -5,13 +5,12 @@ from PIL import Image
 from torchvision import transforms
 
 
-
 def change(raw_img_dir, save_img_dir, raw_label_dir, save_label_dir):
     raw_img_list = os.listdir(raw_img_dir)
     RESIZED_HEIGHT = 640
     RESIZED_WIDTH = 640
     for index, name_with_extention in enumerate(raw_img_list, 1):
-        name = os.path.split(name_with_extention)[0]
+        name = os.path.splitext(name_with_extention)[0]
         image = Image.open(os.path.join(raw_img_dir, name_with_extention))
         resize = transforms.Resize([RESIZED_WIDTH, RESIZED_HEIGHT])
         resized_img = resize(image)
@@ -19,9 +18,15 @@ def change(raw_img_dir, save_img_dir, raw_label_dir, save_label_dir):
         label_save_name = f"{index}.txt"
         resized_img.save(os.path.join(save_img_dir, img_save_name))
         shutil.copy(os.path.join(raw_label_dir, name+'.txt'), os.path.join(save_label_dir, label_save_name))
-    shutil.copy(os.path.join(raw_img_dir, 'classes.txt'), os.path.join(save_label_dir, 'classes.txt'))
+    shutil.copy(os.path.join(raw_label_dir, 'classes.txt'), os.path.join(save_label_dir, 'classes.txt'))
 
-    
+raw_img_dir = '../raw_images/images'
+save_img_dir = '../raw_images/changed_imgs'
+raw_label_dir = '../raw_images/labels'
+save_label_dir = '../raw_images/changed_labels'
+change(raw_img_dir, save_img_dir, raw_label_dir, save_label_dir)
+
+
 
 
 

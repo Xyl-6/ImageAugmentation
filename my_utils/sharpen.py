418389148a6679c7584182342f18012f0bf0c3dc
Xyl-6 <1129563391@qq.com>
2023-03-08 21:40:19 +0800

    before_cutout

--- my_utils/sharpen.py
+++ my_utils/sharpen.py
@@ -13,3 +13,11 @@ def sharpen(input_img_dir, save_img_dir, input_label_dir, save_label_dir, name):
     save_label_path = os.path.join(save_label_dir, name + f"_sharpen.txt")
     output_img.save(save_img_path)
     shutil.copy(input_label_path, save_label_path)
+
+#
+# input_img_dir = '../raw_images/changed_imgs'
+# input_label_dir = '../raw_images/changed_labels'
+# save_img_dir = '../aug_images/images'
+# save_label_dir = '../aug_images/labels'
+# name = '1'
+# sharpen(input_img_dir, save_img_dir, input_label_dir, save_label_dir, name)

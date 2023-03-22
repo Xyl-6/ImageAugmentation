7e3320010aaa754b4bf9dc7df88bf1b138666a65
Xyl-6 <1129563391@qq.com>
2023-03-22 16:54:47 +0800

    final_version

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import os
-import cv2
 import shutil
 from my_utils.change import *
 from my_utils.blur import *
@@ -17,14 +16,16 @@ def copyto(input_img_path, input_label_path, save_img_dir, save_label_dir, save_
 
 
 if __name__ == '__main__':
-    # input_img_dir = './raw_images/changed_imgs'
-    # input_label_dir = './raw_images/changed_labels'
-    # save_img_dir = './aug_images/images'
-    # save_label_dir = 'aug_images/labels'
+
     raw_img_dir = './raw_images/images'
     save_img_dir = './raw_images/changed_imgs'
     raw_label_dir = './raw_images/labels'
     save_label_dir = './raw_images/changed_labels'
+
+    if os.path.exists(save_img_dir) == 0:
+        os.mkdir(save_img_dir)
+    if os.path.exists(save_label_dir):
+        os.mkdir(save_label_dir)
 
     change(raw_img_dir, save_img_dir, raw_label_dir, save_label_dir)
 
@@ -36,6 +37,11 @@ if __name__ == '__main__':
         input_label_dir = './raw_images/changed_labels'
         save_img_dir = './aug_images/images'
         save_label_dir = 'aug_images/labels'
+
+        if os.path.exists(save_img_dir) == 0:
+            os.mkdir(save_img_dir)
+        if os.path.exists(save_label_dir):
+            os.mkdir(save_label_dir)
 
         blur(input_img_dir, save_img_dir, input_label_dir, save_label_dir, name)
         gussian_blur(input_img_dir, save_img_dir, input_label_dir, save_label_dir, name)
@@ -55,6 +61,11 @@ if __name__ == '__main__':
     final_img_dir = './datasets/images'
     final_label_dir = './datasets/labels'
 
+    if os.path.exists(final_img_dir) == 0:
+        os.mkdir(final_img_dir)
+    if os.path.exists(final_label_dir):
+        os.mkdir(final_label_dir)
+
     img_list = os.listdir(save_img_dir)
     img_num = len(img_list)
     for index, name_with_extention in enumerate(img_list, 1):
@@ -68,16 +79,10 @@ if __name__ == '__main__':
             copyto(input_img_path, input_label_path, final_img_dir, final_label_dir, save_kind='test', name=name)
         else:
             copyto(input_img_path, input_label_path, final_img_dir, final_label_dir, save_kind='val', name=name)
+        shutil.copy(os.path.join('raw_images', 'changed_labels', 'classes.txt'), os.path.join('datasets/labels/train', 'classes.txt'))
+        shutil.copy(os.path.join('raw_images', 'changed_labels', 'classes.txt'), os.path.join('datasets/labels/test', 'classes.txt'))
+        shutil.copy(os.path.join('raw_images', 'changed_labels', 'classes.txt'), os.path.join('datasets/labels/train', 'classes.txt'))
 
-
-
-
-
-# def copyto(input_img_path, input_label_path, save_img_dir, save_label_dir, save_kind, name):
-#     save_img_path = os.path.join(save_img_dir, save_kind, name+'.jpg')
-#     save_label_path = os.path.join(save_label_dir, save_kind, name+'.txt')
-#     shutil.copy(input_img_path, save_img_path)
-#     shutil.copy(input_label_path, save_label_path)
 
 
 
--- test.py
+++
@@ -1,3 +0,0 @@
-import torch
-print(torch.cuda.is_available())
-import cv2
